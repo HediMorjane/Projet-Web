72be3f5cc405c19f661b4346a1b4fdade5c51dd4
mattieugascon <74255423+mattieugascon@users.noreply.github.com>
2022-05-29 17:21:26 +0200

    Delete Projet web Matthieu/Projet-Web directory
    -fichier deja présent dans le projet

--- Projet web Matthieu/Projet-Web/Projet web/contant.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$conn=mysqli_connect('localhost', 'root','') or die(mysqli_error());// Connecter la bdd
-$db_select=mysqli_select_db($conn,'projetweb') or die(mysqli_error());// selectionner la bdd
-
-?>
--- Projet web Matthieu/Projet-Web/README.md
+++
@@ -1,1 +0,0 @@
-Ce dossier comprend l'ensemble des fichiers qui on été fait par le groupe depuis le début de la piscine. Il y a en plus un rajout pour les fonctionnalités tout parcourir
